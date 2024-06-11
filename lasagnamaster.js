0ec05ce4e8a401d7244a7bbfc7f56081ce0f3fdc
Raven Hendricks <ravenkbeats@gmail.com>
2024-06-11 13:57:32 -0400

    added comments

--- lasagnamaster.js
+++ lasagnamaster.js
@@ -11,6 +11,16 @@
  * }
  */
 
+/**
+ * Write a function cookingStatus that accepts the remaining time on the timer in minutes as a parameter.
+ * The function has three possible results.
+ *
+ *If the timer shows 0, it should return 'Lasagna is done.'.
+ *If the timer shows any other number, the result should be 'Not done, please wait.'.
+ *If the function is called without a timer value, the result should be 'You forgot to set the timer.'.
+ *The timer will never show a value below 0.
+ */
+
 export function cookingStatus(cookTimeLeft){
     if (cookTimeLeft === 0){
       return 'Lasagna is done.'
@@ -20,6 +30,12 @@ export function cookingStatus(cookTimeLeft){
       return 'You forgot to set the timer.'
     }
   }
+
+ /**
+ * Implement a function preparationTime that accepts an array of layers and the average preparation time per layer in minutes. 
+ * The function should return the estimate for the total preparation time based on the number of layers. 
+ * If the function is called without providing the average preparation time, 2 minutes should be assumed instead.
+ */
   
   export function preparationTime(layers, avgPrepTime){
     if (avgPrepTime === undefined){
